23467e23ca003a19d671768a7dad26dcaeba6f4b
RomainMustang <romain.g@codeur.online>
2017-02-27 16:58:32 +0100

    Add files via upload

---
+++ prog/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+require "vendor/autoload.php";
+
+/*  page par défaut  */
+$page = 'index';
+
+if(isset($_GET['p'])) {
+	$page = $_GET['p'];
+}
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig   = new Twig_Environment($loader, [
+	'cache' => false
+
+]); 
+
+/*  Chargement de la page choisie  */
+switch ($page){
+
+	case 'home':
+		echo $twig->render('views/home.html');
+	break;
+
+	case 'friends':
+		echo $twig->render('views/friends.html');
+	break;
+
+	case 'profil':
+		echo $twig->render('views/profil.html');
+	break;
+
+	default:
+		echo $twig->render('views/index.html');
+	break;
+}
+
+?>
