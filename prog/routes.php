2f1f8100b2a2ef80779109eb3488708a7044bef6
RomainMustang <romain.g@codeur.online>
2017-02-28 10:47:41 +0100

    Add files via upload
    Modification de toutes les routes

--- prog/routes.php
+++ prog/routes.php
@@ -2,42 +2,48 @@
 
 require "vendor/autoload.php";
 
-function road($controller, $page){
+/*  Utilise le controller que l'on a spécifié  */
+function road($controller, $action){
 
-	/*  Appel du controller spécifier  */
 	require_once('controllers/' . $controller . '_controller.php');
 
-	/*  page par défaut  */
-	$page = 'index';
-
-	if(isset($_GET['p'])) {
-		$page = $_GET['p'];
-	}
-
-	$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
-	$twig   = new Twig_Environment($loader, [
-		'cache' => false
-	]); 
-
-	/*  Chargement de la page choisie  */
-	switch ($page){
-
-		case 'home':
-			echo $twig->render('views/home.html');
+	switch($controller){
+		case 'pages':
+			$controller = new PagesController();
 		break;
-
-		case 'friends':
-			echo $twig->render('views/friends.html');
-		break;
-
-		case 'profil':
-			echo $twig->render('views/profil.html');
-		break;
-
-		default:
-			echo $twig->render('views/index.html');
+		case 'posts':
+			$controller = new PostsController();
 		break;
 	}
+	$controller->{ $action }();
+}
+
+/*  Toutes les pages du modele MVC  */
+$controllers = array('pages' => ['index', 'error'],
+					 'posts' => ['home', 'friends', 'profil']);
+
+/*  Vérifie si le controller fait partie de la liste de controller  */
+if (array_key_exists($controller, $controllers)) {
+	
+	/*  Vérifie si l'action est dans la liste du controller  */
+	if (in_array($action, $controllers[$controller])) {
+    
+    /*  Appel le controller  */
+    call($controller, $action);
+    } 
+    
+    /*  Sinon affiche une page d'erreur  */
+    else {
+    
+      call('pages', 'error');
+    
+    }
+} 
+
+else {
+
+	call('pages', 'error');
+
 }
 
 ?>
