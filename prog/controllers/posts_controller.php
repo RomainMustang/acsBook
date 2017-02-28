6322e653ab6e2e49723e182e68ce972912a9917f
Clément <xSlOom@users.noreply.github.com>
2017-02-28 16:57:20 +0100

    Update posts_controller.php

--- prog/controllers/posts_controller.php
+++ prog/controllers/posts_controller.php
@@ -1,5 +1,6 @@
 <?php
 class PostsController {
+    public $nom, $prenom, $mail, $pwd;
     public function home() {
         global $twig;
         echo $twig->render('home.twig', [
@@ -7,15 +8,19 @@ class PostsController {
             "avatar" => ""
         ]);
     }
-
+    
     public function register() {
         global $twig;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $nom    = isset($_POST["nom"]) ? htmlspecialchars($_POST["nom"]) : "";
-            $pnom   = isset($_POST["prenom"]) ? htmlspecialchars($_POST["prenom"]) : "";
-            $email  = isset($_POST["mail"]) ? htmlspecialchars($_POST["mail"]) : "";
-            $pass   = isset($_POST["pwd"]) ? htmlspecialchars($_POST["pwd"]) : "";
-            $user = new UserModel($nom, $pnom, $email, $pass);
+            foreach($_POST as $key => $value) {
+                $this->$key = htmlspecialchars($value);
+            }
+            $user = new UserModel(
+                $this->nom,
+                $this->prenom,
+                $this->mail,
+                $this->pwd
+            );
             $user->NewUser();
         } else {
             echo $twig->render('accueil.twig');
