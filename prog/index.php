4ef314a6061fcf67f03f7fa915fcafa9e2cc6d75
Clément <xSlOom@users.noreply.github.com>
2017-02-28 13:41:56 +0100

    Create index.php

---
+++ prog/index.php
@@ -0,0 +1,11 @@
+<?php
+include 'vendor/autoload.php';
+
+$datab  = new Connection("localhost", "root", "", "acsbook");
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig   = new Twig_Environment($loader);
+
+$controller = isset($_GET["controller"]) ? $_GET["controller"] : "pages";
+$action     = isset($_GET["action"]) ? $_GET["action"] : "home";
+
+require 'routes.php';
