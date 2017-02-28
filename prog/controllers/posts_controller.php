b557ef8bb8899ce3089fbe3bdb0701f7d7405da8
Clément <xSlOom@users.noreply.github.com>
2017-02-28 16:40:24 +0100

    Update posts_controller.php

--- prog/controllers/posts_controller.php
+++ prog/controllers/posts_controller.php
@@ -1,11 +1,28 @@
 <?php
+class PostsController {
+    public function home() {
+        global $twig;
+        echo $twig->render('home.twig', [
+            "name" => "",
+            "avatar" => ""
+        ]);
+    }
 
-	class PostsController{
+    public function register() {
+        global $twig;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $nom    = isset($_POST["nom"]) ? htmlspecialchars($_POST["nom"]) : "";
+            $pnom   = isset($_POST["prenom"]) ? htmlspecialchars($_POST["prenom"]) : "";
+            $email  = isset($_POST["mail"]) ? htmlspecialchars($_POST["mail"]) : "";
+            $pass   = isset($_POST["pwd"]) ? htmlspecialchars($_POST["pwd"]) : "";
+            $user = new UserModel($nom, $pnom, $email, $pass);
+            $user->NewUser();
+        } else {
+            echo $twig->render('accueil.twig');
+        }
+    }
 
-		public function home() {
-			require_once('templates/views/home.html')
-		}
-
-	}
-
-?>
+    public function error() {
+        die('<h1>404 not found');
+    }
+}
