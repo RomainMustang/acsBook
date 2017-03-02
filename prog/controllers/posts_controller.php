f2094ee7e306262914e06e73348175dfe0c8fa7d
Clément <xSlOom@users.noreply.github.com>
2017-03-02 09:36:01 +0100

    Update posts_controller.php

--- prog/controllers/posts_controller.php
+++ prog/controllers/posts_controller.php
@@ -1,10 +1,11 @@
 <?php
 class PostsController {
     private $nom, $prenom, $email, $pwd;
+
     public function home() {
         global $twig, $user;
         if (!isset($_COOKIE["user_token"])) {
-            echo $twig->render('index.html', [
+            echo $twig->render('accueil.twig', [
                 "error" => "danger",
                 "message" => "Veuillez vous connecter pour accéder à cette page."
             ]);
@@ -12,18 +13,19 @@ class PostsController {
             $cookie = preg_replace("/[^a-zA-Z0-9s]/", "", $_COOKIE["user_token"]);
             if ((!empty($cookie)) && (is_array($user->checkCookie($cookie)))) {
                 $info = $user->checkCookie($cookie);
-                echo $twig->render("home.html", [
+                echo $twig->render("home.twig", [
                     "name" => $info["prenom"],
                     "avatar" => "http://pre14.deviantart.net/e12b/th/pre/i/2012/206/0/6/fb_page_avatar___happy_by_muller_saru-d58lfe4.png"
                 ]);
             } else {
-                echo $twig->render("index.html", [
+                echo $twig->render("accueil.twig", [
                     "error" => "danger",
                     "message" => "Veuillez vous reconnecter pour accéder à cette page."
                 ]);
             }
         }
     }
+
     public function register() {
         global $twig, $user;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -37,9 +39,10 @@ class PostsController {
                 $this->pwd
             );
         } else {
-            echo $twig->render('index.html');
+            echo $twig->render('accueil.twig');
         }
     }
+
     public function login() {
         global $twig, $user;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -51,13 +54,25 @@ class PostsController {
                 $this->pass
             );
         } else {
-            echo $twig->render('login.html');
+            echo $twig->render('login.twig');
         }
     }
+
+    public function logout() {
+        global $twig;
+        if (isset($_COOKIE["user_token"])) {
+            setcookie("user_token", "", time()-3600);
+            echo $twig->render('accueil.twig', [
+                "error" => "info",
+                "message" => "Merci de votre visite, à la prochaine."
+            ]);
+        } else {
+            echo $twig->render('accueil.twig');
+        }
+    }
+
     public function error() {
         global $twig;
-        echo $twig->render('error.html');
+        echo $twig->render('error.twig');
     }
 }
-
-?>
