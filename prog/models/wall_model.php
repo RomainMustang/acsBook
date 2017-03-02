2c24a2a1814ea87f2479ad808027d127ccf99d80
RomainMustang <romain.g@codeur.online>
2017-03-02 11:15:25 +0100

    stocke le message dans la base de données

--- prog/models/wall_model.php
+++ prog/models/wall_model.php
@@ -2,7 +2,7 @@
 
 	class WallModel{
 
-		private $message, $data, $id_user;
+		private $message, $date, $id_user;
 
 		function __construct($msg){
 
@@ -14,6 +14,16 @@
 			$this->message = $msg[0];
 			$this->data    = $msg[1];
 			$this->id_user = $msg[2];
+		}
+
+		public function createMsg($msg){
+			global $datab;
+			$query   = "INSERT INTO posts (message,"."date" .", id_utilisateur) VALUES(:message, :date, :id_utilisateur)";
+			$execute = $datab->pdo->prepare($query);
+        	$execute->bindParam(':message', $this->message);
+        	$execute->bindParam(':date', $this->date);
+        	$execute->bindParam(':id_utilisateur', $this->id_user);
+        	return $execute->execute();
 		}
 
 	}
