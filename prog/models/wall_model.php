012f89ed1f499bcfdcadc8aeb0c82b644bcdce12
SarahConnor70 <sarah.p@codeur.online>
2017-03-02 14:30:36 +0100

    Add files via upload

--- prog/models/wall_model.php
+++ prog/models/wall_model.php
@@ -1,31 +1,34 @@
 <?php
 
-	class WallModel{
+class WallModel{
 
-		private $message, $date, $id_user;
+	private $message, $date, $id_user;
 
-		function __construct($msg){
 
-			$this->setMsg($msg);
+	function createMsg($msg){
 
-		} 
+		$this->setMsg($message);
+	} 
 
-		private function setMsg($msg){
-			$this->message = $msg[0];
-			$this->data    = $msg[1];
-			$this->id_user = $msg[2];
-		}
-
-		public function createMsg($msg){
-			global $datab;
-			$query   = "INSERT INTO posts (message,"."date" .", id_utilisateur) VALUES(:message, :date, :id_utilisateur)";
-			$execute = $datab->pdo->prepare($query);
-        	$execute->bindParam(':message', $this->message);
-        	$execute->bindParam(':date', $this->date);
-        	$execute->bindParam(':id_utilisateur', $this->id_user);
-        	return $execute->execute();
-		}
-
+	private function setMsg($msg){
+		$this->message = $msg[0];
+		$this->date = $msg[1];
+		$this->id_user = $msg[2];
 	}
 
-?>
+	public function stockMsg($msg) {
+        global $datab;
+        $query      = "INSERT INTO posts (message, date, id_utilisateur) VALUES(:message, NOW(), :id_utilisateur)";
+        $execute    = $datab->pdo->prepare($query);
+        $execute->bindParam(':message', $this->message);
+        $execute->bindParam(':id_utilisateur', $this->id_user);
+        return $execute->execute();
+	}
+
+	public function getMsg($msg){
+		 global $datab;
+        $query  = $datab->pdo->query("SELECT * from posts where message = '{$msg}'");
+        $fetch  = $query->fetch();
+        return count($fetch) > 1 ? $fetch : false;
+	}
+}
