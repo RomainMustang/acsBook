1c6dd9951cf2237eee1a6154479e469740e7ad2c
SarahConnor70 <sarah.p@codeur.online>
2017-03-02 11:11:58 +0100

    Add files via upload

--- prog/models/user_model.php
+++ prog/models/user_model.php
@@ -1,91 +1,151 @@
 <?php
-class UserModel{
-	private $nom, $prenom, $email, $pass, $error = [];
-/*-------------------------------------------------------*/
-/*-------------------------------------------------------*/
-	public funtion __construct($n, $pn, $e, $p){
-		$this->nom     = $n;
-		$this->prenom  = $pn;
-		$this->email   = $e;
-		$this->pass    = $p;
-		$this->CryPass = $this->createPassword($p);
-	}
-/*-------------------------------------------------------*//*-------------------------------------------------------*/
-	public function NewUser(){
-		global $twig;
-		if ((empty($this->nom)) || (empty($this->prenom)) || (empty($this->email)) || (empty($this->pass))) {
-			$this->error = [
-				'error' => 'danger',
-				'message' => 'Vous devez remplir tous les champs'
-			];
-		}else if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+class UserModel {
+    private $nom, $prenom, $email, $pass, $CryPass, $error = [];
 
-			$this->error = [
+    public function setRegister($n, $pn, $e, $p) {
+        $this->nom      = $n;
+        $this->prenom   = $pn;
+        $this->email    = $e;
+        $this->pass     = $p;
+        $this->CryPass  = $this->createPassword($p);
+        $this->NewRegister();
+    }
 
-				'error' => 'danger',
-				'message' => "L'adresse email doit être valide.", 
-				'data' => [
+    public function setLogin($email, $password) {
+        $this->email    = $email;
+        $this->pass     = $password;
+        $this->CryPass  = $this->createPassword($password);
+        $this->newLogin();
+    }
 
-					'nom'=> $this->nom,
-					'prenom'=>$this->prenom,
-					'pass' =>$this->pass
-				]
-			];
-		}else if (strlen($this->pass)<= 6) {
+    public function newLogin() {
+        global $twig;
+        if ((empty($this->email)) || (empty($this->pass))) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => 'Tu dois remplir tous les champs'
+            ];
+            echo $twig->render('login.html', $this->error);
+        } else if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => "L'adresse email doit être valide.",
+                'data' => [
+                    'pass' => $this->pass
+                ]
+            ];
+            echo $twig->render('login.html', $this->error);
+        } else if (!is_array($this->userInfo($this->email))) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => "L'adresse email est associée à aucun compte."
+            ];
+            echo $twig->render('login.html', $this->error);
+        } elseif (!$this->checkPass($this->email, $this->pass)) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => "Le mot de passe est incorrect."
+            ];
+            echo $twig->render('/templates/views/login.html', $this->error);
+        } else {
+            $this->setToken($this->email);
+            $this->error = [
+                'error' => 'success',
+                'message' => "Vous êtes maintenant connectés sur le site!"
+            ];
+            echo $twig->render('home.html', [
+                "name" => $this->userInfo($this->email)["prenom"],
+                "avatar" => "http://pre14.deviantart.net/e12b/th/pre/i/2012/206/0/6/fb_page_avatar___happy_by_muller_saru-d58lfe4.png"
+            ]);
+        }
+    }
 
-			$this->error = [
-				'error' => 'danger',
-				'message' => 'Votre mot de passe doit être supérieur à 6 caractères.',
-				'data' =>[
-					'nom' =>$this->nom,
-					'prenom'=>$this->prenom,
-					'pass'=>$this->pass
-				]
-			];
-		}else if ($this->emailTaken($this->email)) {
-			$this->error = [
-				'error' => 'danger',
-				'message' => 'Cette adresse mail est déjà utilisée!',
-				'data' => [
-					'nom'=>$this->nom,
-					'prenom'=>$this->prenom,
-					'pass'=>$this->pass
+    public function NewRegister() {
+        global $twig;
+        if ((empty($this->nom)) || (empty($this->prenom)) || (empty($this->email)) || (empty($this->pass))) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => 'Tu dois remplir tous les champs'
+            ];
+        } else if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => "L'adresse email doit être valide.",
+                'data' => [
+                    'nom' => $this->nom,
+                    'prenom' => $this->prenom,
+                    'pass' => $this->pass
+                ]
+            ];
+        } else if (strlen($this->pass) <= 6) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => 'Votre mot de passe doit être supérieur à 6 caractères.',
+                'data' => [
+                    'nom' => $this->nom,
+                    'prenom' => $this->prenom,
+                    'mail' => $this->email
+                ]
+            ];
+        } else if (is_array($this->userInfo($this->email))) {
+            $this->error = [
+                'error' => 'danger',
+                'message' => 'Cette adresse email est déjà prise!',
+                'data' => [
+                    'nom' => $this->nom,
+                    'prenom' => $this->prenom,
+                    'pass' => $this->pass
+                ]
+            ];
+        } else {
+            $this->insert();
+            $this->error = [
+                'error' => 'success',
+                'message' => 'Votre compte a bien été créée!'
+            ];
+        }
+        echo $twig->render('/templates/views/index.html', $this->error);
+    }
 
-				]
-			];
-		} else {
+    public function insert() {
+        global $datab;
+        $query      = "INSERT INTO utilisateurs (nom, prenom, mail, password) VALUES(:nom, :prenom, :mail, :password)";
+        $execute    = $datab->pdo->prepare($query);
+        $execute->bindParam(':nom', $this->nom);
+        $execute->bindParam(':prenom', $this->prenom);
+        $execute->bindParam(':mail', $this->email);
+        $execute->bindParam(':password', $this->CryPass);
+        return $execute->execute();
+    }
 
-			$this->insert();
-			$this->error = [
-				'error' => 'success',
-				'message' => 'Votre compte a bien été créer ! '
-			];
-		}
-		echo $twig->render('index.html', $this->error);
-	}
-/*-------------------------------------------------------*/
-/*-------------------------------------------------------*/
-	public function insert(){
+    public function userInfo($email) {
+        global $datab;
+        $query  = $datab->pdo->query("SELECT * from utilisateurs where mail = '{$email}'");
+        $fetch  = $query->fetch();
+        return count($fetch) > 1 ? $fetch : false;
+    }
 
-		global $datab;
+    public function setToken($email) {
+        global $datab;
+        $token = bin2hex(random_bytes(30));
+        setcookie('user_token', $token, time() + 3600);
+        $datab->pdo->query("update utilisateurs set token = '{$token}' where mail = '{$email}'");
+    }
 
-		$query = "INSERT INTO utilisateurs (nom,prenom, mail,password) VALUES (:nom, :prenom, :mail, :password)";
-		$execute = $datab->pdo->prepare($query);
-		$execute->bindParam(':nom', $this->nom);
-		$execute->bindParam(':prenom', $this->prenom);
-		$execute->bindParam(':mail', $this->mail);
-		$execute->bindParam(':password', $this->Crypass);
-		return $execute->execute();
-	}
+    public function checkCookie($cookie) {
+        global $datab;
+        $query  = $datab->pdo->query("SELECT * from utilisateurs where token = '{$cookie}'");
+        $fetch  = $query->fetch();
+        return count($fetch) > 1 ? $fetch : false;
+    }
 
-/*-------------------------------------------------------*/
-/*-------------------------------------------------------*/
+    public function createPassword($pass) {
+        return password_hash($pass, PASSWORD_DEFAULT);
+    }
 
-public function emailTaken($email){
+    public function checkPass($email, $pass) {
+        $info = $this->userInfo($email);
+        return password_verify($pass, $info["password"]) ? true : false;
+    }
 
-	global $datab;
-	$query = $datab->pdo->$query("SELECT * from utilisateurs where mail = '{$email}'");
-	$fetch = $query->fetch();
-	return count ($fetch) > 1 ? true :false;
-	}
 }
--- prog/routes.php
+++ prog/routes.php
@@ -1,25 +1,46 @@
 <?php
-
 /*  Utilise le controller que l'on a spécifié  */
 function road($controller, $action){
-
 	require_once('controllers/' . $controller . '_controller.php');
-
 	switch($controller){
 		case 'pages':
 			$controller = new PagesController();
 		break;
 		case 'posts':
+			require_once('models/friends_model.php');
+			require_once('models/post_model.php');
+			require_once('models/user_model.php');
+			require_once('models/wall_model.php');
 			$controller = new PostsController();
 		break;
 	}
 	$controller->{ $action }();
 }
-
 /*  Toutes les pages du modele MVC  */
 $controllers = array('pages' => ['index', 'error'],
-					 'posts' => ['home', 'friends', 'profil']);
-
+					 'posts' => ['home', 'friends', 'profil', 'register', 'login']);
+$models = [
+    "friend" => "friends",
+    "wall" => "wall",
+    "post" => "post",
+    "user" => "user"
+];
+foreach($models as $key => $value) {
+    require 'models/' . $value . '_model.php';
+    if (class_exists(ucfirst($value)."Model")) {
+        switch($key) {
+            case "user":
+                $user = new UserModel();
+                break;
+            case "friend":
+                $friend = new FriendsModel();
+                break;
+            default:
+                die('Failed to load some class');
+                break;
+        }
+    }
+}
 /*  Vérifie si le controller fait partie de la liste de controller  */
 if (array_key_exists($controller, $controllers)) {
 	
@@ -37,11 +58,7 @@ if (array_key_exists($controller, $controllers)) {
     
     }
 } 
-
 else {
-
 	road('pages', 'error');
-
 }
-
 ?>
