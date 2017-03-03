ae35f704b37b0707e8d6855515ab1b2b500aec04
Clément <xSlOom@users.noreply.github.com>
2017-03-03 10:32:17 +0100

    mise à jour wall_model avec nouvelles fonctions

--- prog/models/wall_model.php
+++ prog/models/wall_model.php
@@ -1,30 +1,32 @@
 <?php
-
-	class WallModel{
-
-		private $message, $date, $id_user;
-
-		public function createMsg($msg){
-
-			$this->setMsg($msg);
-
-		} 
-
-		private function setMsg($msg){
-			$this->message = $msg[0];
-			$this->date    = $msg[1];
-			$this->id_user = $msg[2];
-		}
-
-		public function stockMsg($msg){
-			global $datab;
-			$query   = "INSERT INTO posts (message,date, id_utilisateur) VALUES(:message, NOW(), :id_utilisateur)";
-			$execute = $datab->pdo->prepare($query);
-        	$execute->bindParam(':message', $this->message);
-        	$execute->bindParam(':id_utilisateur', $this->id_user);
-        	return $execute->execute();
-		}
-
-	}
-
-?>
+class WallModel{
+    public $message, $date, $id_user;
+    public function setMsg($message, $id) {
+        $this->message  = $message;
+        $this->date     = time();
+        $this->id_user  = $id;
+        $this->stockMsg();
+    }
+    public function stockMsg() {
+        global $datab;
+        $query      = "INSERT INTO posts (message, date, id_utilisateur) VALUES(:message, NOW(), :id_utilisateur)";
+        $execute    = $datab->pdo->prepare($query);
+        $execute->bindParam(':message', $this->message);
+        $execute->bindParam(':id_utilisateur', $this->id_user);
+        return $execute->execute();
+    }
+    public function getMsg($id){
+        global $datab;
+        $info   = [];
+        $query  = $datab->pdo->query("SELECT * from posts where id_utilisateur = '{$id}'");
+        $fetch  = $query->fetchAll();
+        foreach($fetch as $key => $value) {
+            $info[] = [
+                "id" => $value["id"],
+                "message" => $value["message"],
+                "date" => $value["date"]
+            ];
+        }
+        return sizeof($info)  == 0 ? false : print json_encode($info);
+    }
+}
