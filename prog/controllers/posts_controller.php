38998bfbd4d3dc4c601d90cb84a280803ad32bae
Clément <xSlOom@users.noreply.github.com>
2017-03-06 10:32:51 +0100

    POSTS IS BACK

--- prog/controllers/posts_controller.php
+++ prog/controllers/posts_controller.php
@@ -1,30 +1,44 @@
 <?php
 class PostsController {
-    private $nom, $prenom, $email, $pwd;
+    private $nom, $prenom, $email, $pwd, $message, $id, $id1, $id2, $option;
+
     public function home() {
-        global $twig, $user;
-        if (!isset($_COOKIE["user_token"])) {
-            echo $twig->render('templates/views/index.html', [
+        global $twig, $user, $wall;
+        if ($this->isLogged() == false) {
+            echo $twig->render("templates/views/index.html", [
                 "error" => "danger",
-                "message" => "Veuillez vous connecter pour accéder à cette page."
+                "message" => "Veuillez vous connecter/enregistrer pour accéder à cette page."
             ]);
         } else {
             $cookie = preg_replace("/[^a-zA-Z0-9s]/", "", $_COOKIE["user_token"]);
-            if ((!empty($cookie)) && (is_array($user->checkCookie($cookie)))) {
-                $info = $user->checkCookie($cookie);
-                echo $twig->render("templates/views/home.html", [
-                    "id" => $info["id"],
-                    "name" => $info["prenom"],
-                    "avatar" => $info["photos"]
-                ]);
-            } else {
-                echo $twig->render("templates/views/index.html", [
-                    "error" => "danger",
-                    "message" => "Veuillez vous reconnecter pour accéder à cette page."
-                ]);
-            }
+            $info = $user->checkCookie($cookie);
+            echo $twig->render("home.twig", [
+                "id" => $info["id"],
+                "name" => $info["prenom"],
+                "avatar" => $info["photos"]
+            ]);
         }
     }
+
+    public function profil() {
+        global $twig, $user, $wall;
+        if ($this->isLogged() == false) {
+            echo $twig->render("templates/views/index.html", [
+                "error" => "danger",
+                "message" => "Veuillez vous connecter/enregistrer pour accéder à cette page."
+            ]);
+        } else {
+            $cookie = preg_replace("/[^a-zA-Z0-9s]/", "", $_COOKIE["user_token"]);
+            $info = $user->checkCookie($cookie);
+            echo $twig->render("templates/views/profil.html", [
+                "id" => $info["id"],
+                "name" => $info["prenom"],
+                "avatar" => $info["photos"]
+                //"mur" => $wall->getMsg($info["id"])
+            ]);
+        }
+    }
+
     public function register() {
         global $twig, $user;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -41,6 +55,7 @@ class PostsController {
             echo $twig->render('templates/views/index.html');
         }
     }
+
     public function login() {
         global $twig, $user;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -55,18 +70,33 @@ class PostsController {
             echo $twig->render('templates/views/login.html');
         }
     }
+
     public function logout() {
         global $twig;
         if (isset($_COOKIE["user_token"])) {
             setcookie("user_token", "", time()-3600);
-            echo $twig->render('accueil.twig', [
+            echo $twig->render('templates/views/index.html', [
                 "error" => "info",
                 "message" => "Merci de votre visite, à la prochaine."
             ]);
         } else {
-            echo $twig->render('accueil.twig');
+            echo $twig->render('templates/views/index.html');
         }
     }
+
+    public function isLogged() {
+        global $user;
+        if (!isset($_COOKIE["user_token"])) {
+            return false;
+        } else {
+            $cookie = preg_replace("/[^a-zA-Z0-9s]/", "", $_COOKIE["user_token"]);
+            if ((empty($cookie)) && (!is_array($user->checkCookie($cookie)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public function wall() {
         global $wall;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -79,6 +109,7 @@ class PostsController {
             );
         }
     }
+
     public function wallView() {
         global $wall;
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -100,10 +131,33 @@ class PostsController {
             ], JSON_PRETTY_PRINT));
         }
     }
+
+    public function friends() {
+        global $friend;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            foreach($_POST as $key => $value) {
+                $this->$key = htmlspecialchars($value);
+            }
+            if (sizeof($_POST) == 1) { // notifications
+                $friend->getNotif($this->id);
+            } else { // someone accepted/refuse a request.
+                $friend->setRequest(
+                    $this->id1,
+                    $this->id2,
+                    $this->option,
+                    false
+                );
+            }
+        } else {
+            die(json_encode([
+                'error' => true,
+                'message' => 'GET not supported'
+            ], JSON_PRETTY_PRINT));
+        }
+    }
+
     public function error() {
         global $twig;
         echo $twig->render('templates/views/error.html');
     }
 }
-
-?>
