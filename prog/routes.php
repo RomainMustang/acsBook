a1024fc788f06b41e59c5b58f3167a1f4c42e989
SarahConnor70 <sarah.p@codeur.online>
2017-03-06 16:41:55 +0100

    Add files via upload

--- prog/routes.php
+++ prog/routes.php
@@ -1,24 +1,40 @@
 <?php
 /*  Utilise le controller que l'on a spécifié  */
 function road($controller, $action){
-	require_once('controllers/' . $controller . '_controller.php');
-	switch($controller){
-		case 'pages':
-			$controller = new PagesController();
-		break;
-		case 'posts':
-			require_once('models/friends_model.php');
-			require_once('models/post_model.php');
-			require_once('models/user_model.php');
-			require_once('models/wall_model.php');
-			$controller = new PostsController();
-		break;
-	}
-	$controller->{ $action }();
+    require_once('controllers/' . $controller . '_controller.php');
+    switch($controller){
+        case 'pages':
+            $controller = new PagesController();
+        break;
+        case 'posts':
+            require_once('models/friends_model.php');
+            require_once('models/post_model.php');
+            require_once('models/user_model.php');
+            require_once('models/wall_model.php');
+            $controller = new PostsController();
+        break;
+    }
+    $controller->{ $action }();
 }
 /*  Toutes les pages du modele MVC  */
-$controllers = array('pages' => ['index', 'error'],
-					 'posts' => ['home', 'friends', 'profil', 'register', 'login']);
+$controllers = [
+    "pages" => [
+        "index",
+        "error"
+    ],
+    "posts" => [
+        "home",
+        "search",
+        "login",
+        "register",
+        "profil",
+        "friends",
+        "friend",
+        "logout",
+        "wall",
+        "wallView"
+    ]
+];
 $models = [
     "friend" => "friends",
     "wall" => "wall",
@@ -35,17 +51,20 @@ foreach($models as $key => $value) {
             case "friend":
                 $friend = new FriendsModel();
                 break;
+            case "wall":
+                $wall = new WallModel();
+                break;
             default:
-                die('Failed to load some class');
+                die('Failed to load some class'.$key);
                 break;
         }
     }
 }
 /*  Vérifie si le controller fait partie de la liste de controller  */
 if (array_key_exists($controller, $controllers)) {
-	
-	/*  Vérifie si l'action est dans la liste du controller  */
-	if (in_array($action, $controllers[$controller])) {
+    
+    /*  Vérifie si l'action est dans la liste du controller  */
+    if (in_array($action, $controllers[$controller])) {
     
     /*  Appel le controller  */
     road($controller, $action);
@@ -59,6 +78,6 @@ if (array_key_exists($controller, $controllers)) {
     }
 } 
 else {
-	road('pages', 'error');
+    road('pages', 'error');
 }
 ?>
