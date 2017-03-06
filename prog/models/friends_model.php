2fd799e8f607dda38b246c706237a577f7a03256
Clément <xSlOom@users.noreply.github.com>
2017-03-06 10:36:24 +0100

    Update friends_model.php

--- prog/models/friends_model.php
+++ prog/models/friends_model.php
@@ -1,9 +1,48 @@
 <?php
+class FriendsModel {
+    public $id1, $id2, $option;
 
-	class Friends{
+    public function setRequest($id1, $id2, $option, $update = false) {
+        $this->id1 = $id1;
+        $this->id2 = $id2;
+        $this->option = $option;
+        $update == true ? $this->saveRequest() : $this->createRequest();
+    }
 
-		
-		
-	}
+    public function saveRequest() {
+        global $datab;
+        $query = $datab->pdo->prepare("UPDATE amis SET valeur = :opt WHERE id_ami1 = :id1 and id_ami2 = :id2");
+        $query->bindParam(":opt", $this->option);
+        $query->bindParam(":id1", $this->id1);
+        $query->bindParam(":id2", $this->id2);
+        $query->execute();
+        return print json_encode([
+            "error" => false
+        ]);
+    }
 
-?>
+    public function getNotif($id) {
+        global $datab;
+        $query  = $datab->pdo->prepare("SELECT * from amis where id_ami1 = :id and valeur = '1' ORDER by id DESC");
+        $query->bindParam(':id', $id);
+        $query->execute();
+        $fetch  = $query->fetchAll();
+        $info   = ["friendcount" => sizeof($fetch)];
+        foreach($fetch as $key => $value) {
+            $info[] = [
+                "requester" => $value["id_ami2"]
+            ];
+        }
+        return sizeof($info)  == 0 ? false : print json_encode($info);
+    }
+
+    public function getFriendStatus($id, $id2) {
+        global $datab;
+        $query = $datab->pdo->prepare("SELECT * FROM amis WHERE id_ami1 = :id1 and id_ami2 = :id2");
+        $query->bindParam(":id1", $id);
+        $query->bindParam(":id2", $id2);
+        $query->execute();
+        $fetch = $query->fetch();
+        return sizeof($fetch) > 1 && $fetch["valeur"] == 2 ? true : false;
+    }
+}
